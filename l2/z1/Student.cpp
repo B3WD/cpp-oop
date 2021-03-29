1e179d9c98745ad821be07d1484aa71ffc545473
Tsvetomir Pavlov <tsvetomir.kpavlov@gmail.com>
2021-03-29 22:40:23 +0300

    Add base class

--- l2/z1/Student.cpp
+++ l2/z1/Student.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Student.h"
+#include <cstring>
+
+Student::~Student() {
+
+}
+
+Student::Student() {
+
+}
+
+Student::Student(char *name, char *fn, int age, int avgGrade, int classYear) {
+
+}
+
+Student::Student(const Student &rhs) {
+    _name = new char[strlen(rhs._name)+1];
+    strcpy(_name, rhs._name);
+
+    _fn = new char[strlen(rhs._name)+1];
+    strcpy(_name, rhs._name);
+
+    _age = rhs._age;
+    _avgGrade = rhs._avgGrade;
+    _classYear = rhs._classYear;
+}
+
+Student &Student::operator=(const Student &rhs) {
+
+    return <#initializer#>;
+}
--- l2/z1/Student.h
+++ l2/z1/Student.h
@@ -8,9 +8,19 @@
 
 class Student {
 private:
+    char *_name;
+    char *_fn;
+    int _age;
+    int _avgGrade;
+    int _classYear;
 
 public:
-    
+    Student();
+    Student(char* name, char* fn, int age, int avgGrade, int classYear);
+    Student(const Student& rhs);
+    ~Student();
+    Student& operator=(const Student& rhs);
+
 };
 
 
