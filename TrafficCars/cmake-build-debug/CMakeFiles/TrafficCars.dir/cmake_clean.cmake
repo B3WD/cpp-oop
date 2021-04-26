355efcd487e9fa8a7ee464e460e63c59d440d778
Tsvetomir Pavlov <tsvetomir.kpavlov@gmail.com>
2021-04-26 09:08:49 +0300

    init

---
+++ TrafficCars/cmake-build-debug/CMakeFiles/TrafficCars.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrafficCars.dir/main.cpp.obj"
+  "TrafficCars.exe"
+  "TrafficCars.exe.manifest"
+  "TrafficCars.pdb"
+  "libTrafficCars.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TrafficCars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TrafficCars/cmake-build-debug/CMakeFiles/TrafficCars.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TrafficCars.
+# This may be replaced when dependencies are built.
