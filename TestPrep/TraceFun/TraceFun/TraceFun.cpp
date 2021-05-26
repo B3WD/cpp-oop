d8c8cdf94829c31470377e1ac9c4489369e12701
Tsvetomir Pavlov <tsvetomir.kpavlov@gmail.com>
2021-05-26 17:18:50 +0300

    TraceFun done

---
+++ TestPrep/TraceFun/TraceFun/TraceFun.cpp
@@ -0,0 +1,13 @@
+﻿#include "TraceFun.h"
+
+TraceFun::TraceFun(const char* name, std::ostream& out) :_name(new char[strlen(name) + 1]), _out(out)
+{
+	strcpy(_name, name);
+	_out << _name << " starts here.\n";
+}
+
+TraceFun::~TraceFun()
+{
+	_out << _name << " ends here.\n";
+	delete _name;
+}
---
+++ TestPrep/TraceFun/TraceFun/TraceFun.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <iostream>
+
+class TraceFun {
+private:
+	char* _name;
+	std::ostream& _out;
+
+public:
+	TraceFun(const char* name = "null", std::ostream& out = std::cout);
+	~TraceFun();
+};
